bcb8cb9f9cc67f9b9d17f8cfeeab7084db4c8816
Kodama Naoyuki <mmaga@dstribe.net>
2026-06-30 08:26:06 +0900

    feat: detektの追加
    ref: 12

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,26 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization")
     id("org.jetbrains.intellij.platform")
     id("org.jetbrains.changelog")
+    alias(libs.plugins.detekt)
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     testImplementation("junit:junit:4.13.2")
+    detektPlugins(libs.detektFormatting)
+
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2025.2.6.2")
         testFramework(TestFrameworkType.Platform)
     }
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom("$projectDir/config/detekt/detekt.yml")
+    autoCorrect = true
+    parallel = true
 }
